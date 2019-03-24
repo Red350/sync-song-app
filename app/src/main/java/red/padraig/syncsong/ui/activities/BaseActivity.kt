74ab8ca74f67e9072b98cf0a17123b4f98ad6d89
Padraig Redmond <pwredmond@gmail.com>
2019-03-24 00:27:10 +0000

    Changed app theme to dark no action bar.
    Updated startup screen to display app name in fancy font.

--- app/src/main/java/red/padraig/syncsong/ui/activities/BaseActivity.kt
+++ app/src/main/java/red/padraig/syncsong/ui/activities/BaseActivity.kt
@@ -1,7 +1,9 @@
 package red.padraig.syncsong.ui.activities
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import com.android.volley.RequestQueue
 import red.padraig.syncsong.SharedPrefsWrapper
 import red.padraig.syncsong.SyncSongApplication
@@ -18,4 +20,8 @@ abstract class BaseActivity : AppCompatActivity() {
         volleyQueue = (application as SyncSongApplication).volleyQueue
     }
 
+    // Set a custom font for a text view, using a font from the assets/fonts directory.
+    fun setFont(tv: TextView, fontName: String) {
+        tv.typeface = Typeface.createFromAsset(assets, "fonts/$fontName")
+    }
 }
