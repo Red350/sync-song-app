f73f523e7948cb1ba9774cf6dfd35fba7c9c5837
Padraig Redmond <pwredmond@gmail.com>
2019-03-27 00:51:21 +0000

    Lobby now joined on create

--- app/src/main/java/red/padraig/syncsong/ui/activities/CreateLobbyActivity.kt
+++ app/src/main/java/red/padraig/syncsong/ui/activities/CreateLobbyActivity.kt
@@ -1,5 +1,6 @@
 package red.padraig.syncsong.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -22,15 +23,19 @@ class CreateLobbyActivity : BaseActivity() {
 
     private fun initListeners() {
         createlobby_btn_create.setOnClickListener {
+            val name = createlobby_et_name.text.toString()
             syncSongAPI.createLobby(
-                    createlobby_et_name.text.toString(),
+                    name,
                     createlobby_spin_genre.selectedItem.toString(),
                     createlobby_rg_pub.isChecked,
                     sharedPrefs.username,
-                    Response.Listener { response ->
-                        Log.d(this.tag(), "Create lobby response: $response")
-                        Toast.makeText(this, response, Toast.LENGTH_SHORT).show()
-                        onBackPressed()
+                    Response.Listener { id ->
+                        Log.d(this.tag(), "Create lobby response: $id")
+                        Toast.makeText(this, "Created lobby with ID: $id", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, LobbyActivity::class.java)
+                        intent.putExtra("LOBBY_ID", id)
+                        intent.putExtra("LOBBY_NAME", name)
+                        startActivity(intent)
                     }
             )
         }
